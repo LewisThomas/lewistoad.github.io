568f4b8b985aa8826bee0d8f1d117a08cbae828e
Thomas Lewis <thomasbartlewis@gmail.com>
2020-03-02 01:03:51 -0700

    remake site

---
+++ precache-manifest.28d94107f815eca1ce28bdabc839ad78.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314855c66675a4480d872a41d34c4047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6787f018a8a084355443",
+    "url": "/static/css/main.5996c61e.chunk.css"
+  },
+  {
+    "revision": "029adfee30a324ff8392",
+    "url": "/static/js/2.00bdc575.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/static/js/2.00bdc575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6787f018a8a084355443",
+    "url": "/static/js/main.74b04380.chunk.js"
+  },
+  {
+    "revision": "3b0f5f662a0cbfad28a0",
+    "url": "/static/js/runtime-main.bfb245bc.js"
+  },
+  {
+    "revision": "e959ae792bc5168a434db5ff46bc598d",
+    "url": "/static/media/Bunnies.e959ae79.jpg"
+  },
+  {
+    "revision": "e60357c006113a0d7b255a35163e42cb",
+    "url": "/static/media/Catan.e60357c0.jpg"
+  },
+  {
+    "revision": "8018db283547f66dbb29e4c7fd611f39",
+    "url": "/static/media/Dice.8018db28.jpg"
+  },
+  {
+    "revision": "84b1d597402664e150cfda1a431136f6",
+    "url": "/static/media/Uno.84b1d597.png"
+  },
+  {
+    "revision": "ec4d223af546d7cbf9a10213fe2b5890",
+    "url": "/static/media/chess.ec4d223a.jpg"
+  },
+  {
+    "revision": "7fde887a6f8679ded0257f4ceb74e987",
+    "url": "/static/media/clockIcon.7fde887a.svg"
+  },
+  {
+    "revision": "5912285f4d6f0009c1b3945ab6f26a2d",
+    "url": "/static/media/monopoly.5912285f.jpg"
+  },
+  {
+    "revision": "4be6df24a1612c90fe3d0a7f345ba81f",
+    "url": "/static/media/nevermore.4be6df24.jpg"
+  },
+  {
+    "revision": "02bb06b827f09ba3b480cd923fa89a96",
+    "url": "/static/media/personIcon.02bb06b8.svg"
+  },
+  {
+    "revision": "3e1a4da629db77515777c99b069e05a9",
+    "url": "/static/media/phase10.3e1a4da6.jpg"
+  },
+  {
+    "revision": "7b69a97de0d4af6c33a35020e414b85d",
+    "url": "/static/media/rook.7b69a97d.jpg"
+  },
+  {
+    "revision": "97a309edb34f597feee009aaccbf5dee",
+    "url": "/static/media/talisman.97a309ed.jpeg"
+  },
+  {
+    "revision": "d39c8eae6c509ea5431ad4a598733421",
+    "url": "/static/media/yahtzee.d39c8eae.jpeg"
+  }
+]);
